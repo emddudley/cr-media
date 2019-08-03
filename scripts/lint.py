25e03bcf78026ec63c3f471290a9f5b2331a1501
Michael Dudley <emddudley@gmail.com>
2019-08-02 22:18:32 -0400

    Add lint script

---
+++ scripts/lint.py
@@ -0,0 +1,16 @@
+"""
+Normalize the data files.
+"""
+
+import json
+from pathlib import Path
+
+
+data_dir = Path(Path(__file__).parent, '..', '_data').resolve()
+
+for data_path in data_dir.glob('*.json'):
+    with open(data_path, 'r') as f:
+        data_json = json.load(f)
+
+    with open(data_path, 'w') as f:
+        json.dump(data_json, f, ensure_ascii=False, indent=2, sort_keys=True)
